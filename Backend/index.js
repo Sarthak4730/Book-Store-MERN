377ca6d579b0aa5060552e94d535f47e78b2124a
github Sarthak4730 <sarthakasm07@gmail.com>
2025-03-15 22:33:10 +0530

    password changed

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 
 async function main(){
-    await mongoose.connect("mongodb+srv://sarthakasm07:W6pp1r9InPuAV9wy@cluster0.b18x2.mongodb.net/book-store?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://sarthakasm07:test123@cluster0.b18x2.mongodb.net/book-store?retryWrites=true&w=majority&appName=Cluster0");
     app.use('/', (req, res) => {
         res.send("book store server");
     });
