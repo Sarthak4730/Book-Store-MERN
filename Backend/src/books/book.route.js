59777c1333658b8e07ef8171dd2c8a0e76e7600e
github Sarthak4730 <sarthakasm07@gmail.com>
2025-03-17 22:23:22 +0530

    post req done correctly, now get to do

--- Backend/src/books/book.route.js
+++ Backend/src/books/book.route.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const Book = require('./book.model');
+
 router.post('/create-book', async (req, res) => {
-    console.log("posted book is:\n", req.body);
+    try {
+        const newBook = await Book( { ...req.body } );
+        await newBook.save();
+        console.error("Success creating book");
+        res.status(200).send( { message: "Book posted successfully", book: newBook } );
+    } catch (error) {
+        console.error("Error creating book: ", error);
+        res.status(500).send( { message: "Book post failed" } );
+    }
 });
 router.get('/get-book', async (req, res) => {
 
