0237454ace3e30829daf3a77e5211c9638aebc29
github Sarthak4730 <sarthakasm07@gmail.com>
2025-03-30 15:21:07 +0530

    need to work on register form

--- Frontend/src/pages/Books/LoginPage.jsx
+++ Frontend/src/pages/Books/LoginPage.jsx
@@ -12,6 +12,8 @@ const LoginPage = () => {
         formState: { errors },
     } = useForm();
     const onSubmit = (data) => console.log(data);
+    
+    // const handleGoogleSignIn = () => {}
 
     return <>
         <Navbar count={cartItems.length} />
@@ -29,6 +31,7 @@ const LoginPage = () => {
             
             <p>Don't have an account? Please <Link to="/register" className="text-blue-500 font-bold">Register</Link> </p>
             
+            {/* <button onClick={handleGoogleSignIn} */}
             <button className="cursor-pointer bg-black w-[25vw] h-[6vh] text-white font-bold rounded-md mt-4 flex justify-center items-center gap-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="currentColor" fillRule="evenodd" d="M17.6 8.5h-7.5v3h4.4c-.4 2.1-2.3 3.5-4.4 3.4c-2.6-.1-4.6-2.1-4.7-4.7c-.1-2.7 2-5 4.7-5.1c1.1 0 2.2.4 3.1 1.2l2.3-2.2C14.1 2.7 12.1 2 10.2 2c-4.4 0-8 3.6-8 8s3.6 8 8 8c4.6 0 7.7-3.2 7.7-7.8c-.1-.6-.1-1.1-.3-1.7" clipRule="evenodd"/></svg>
                 Sign in with Google
--- Frontend/src/pages/Books/RegisterPage.jsx
+++ Frontend/src/pages/Books/RegisterPage.jsx
@@ -2,16 +2,30 @@ import { Link } from "react-router";
 import Navbar from "../../components/Navbar";
 import { useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
+import { useAuth } from "../../context/AuthContext";
 
 const RegisterPage = () => {
     const cartItems = useSelector(state => state.cart.cartItems);
+    const { registerUser } = useAuth();
+
     const {
         register,
         handleSubmit,
         watch,
         formState: { errors },
     } = useForm();
-    const onSubmit = (data) => console.log(data)
+
+    const onSubmit = async (data) => {
+        console.log(data);
+        try {
+            await registerUser(data.email, data.password);
+            alert("Registration Successful");
+        } catch (error) {
+            console.log("invalid email and/or password");
+        }
+    }
+
+    // const handleGoogleSignIn = () => {}
 
     return <>
         <Navbar count={cartItems.length} />
@@ -25,10 +39,11 @@ const RegisterPage = () => {
             <label className="text-lg font-bold" htmlFor="password">Password</label>
             <input {...register("password", { required: true })} className="border-2 border-gray-300 w-[25vw] p-2 rounded-md" type="password" id="password" placeholder="Password"/>
             
-            <button className="cursor-pointer bg-blue-500 w-[5vw] h-[5vh] text-white font-bold rounded-md mt-4" type="submit">Sign Up</button>
+            <button className="cursor-pointer bg-blue-500 w-[5vw] h-[5vh] text-white font-bold rounded-md mt-4" type="submit">Register</button>
             
             <p>Have an account? Please <Link to="/login" className="text-blue-500 font-bold">Login</Link> </p>
             
+            {/* <button onClick={handleGoogleSignIn} */}            
             <button className="cursor-pointer bg-black w-[25vw] h-[6vh] text-white font-bold rounded-md mt-4 flex justify-center items-center gap-3">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path fill="currentColor" fillRule="evenodd" d="M17.6 8.5h-7.5v3h4.4c-.4 2.1-2.3 3.5-4.4 3.4c-2.6-.1-4.6-2.1-4.7-4.7c-.1-2.7 2-5 4.7-5.1c1.1 0 2.2.4 3.1 1.2l2.3-2.2C14.1 2.7 12.1 2 10.2 2c-4.4 0-8 3.6-8 8s3.6 8 8 8c4.6 0 7.7-3.2 7.7-7.8c-.1-.6-.1-1.1-.3-1.7" clipRule="evenodd"/></svg>
                 Sign in with Google
