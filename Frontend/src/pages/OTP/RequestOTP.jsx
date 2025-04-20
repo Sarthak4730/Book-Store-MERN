cba92397a1c76ceb93365f5ee1a65b6f10907c43
github Sarthak4730 <sarthakasm07@gmail.com>
2025-04-20 14:23:02 +0530

    OTP Login success

--- Frontend/src/pages/OTP/RequestOTP.jsx
+++ Frontend/src/pages/OTP/RequestOTP.jsx
@@ -1,19 +1,38 @@
 import { useNavigate } from "react-router";
+import axios from "axios";
+import { useEffect } from "react";
+import { useAuth } from "../../context/AuthContext";
 
 const RequestOTP = () => {
     const navigate = useNavigate();
+    const { currentUser } = useAuth(null);
+    const email = currentUser.providerData[0].email;
+
+    const sendOTP = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/otp/send-otp", {email} );
+            console.log(response.data.message);
+        } catch (err) {
+            console.error("Error sending OTP:", err.response?.data || err.message);
+        }
+    };
 
     const verifyOTP = () => {
         navigate("/otp-verify");
     }
 
-    return <>
-        <h2>As part of 2-step verification - we generate OTP for you, check your Email Inbox and then click on the button given below</h2>
+    useEffect(() => {
+        sendOTP();
+    }, []);
+
+    return <div className="w-[50vw] h-[50vh] mx-auto mt-[25vh] flex flex-col justify-evenly items-center">
+        <h1 className="text-5xl text-blue-500 font-bold">2-Step Verification</h1>
+        <h2 className="text-3xl font-semibold text-center">Check your Email Inbox for the OTP we sent and then click on the button given below</h2>
         
-        <button onClick={verifyOTP}>
+        <button onClick={verifyOTP} className="w-[8vw] h-[6vh] bg-yellow-400 text-xl font-semibold flex justify-center items-center rounded-md py-1 hover:scale-115 cursor-pointer">
             Verify OTP
         </button>
-    </>
+    </div>
 };
 
 export default RequestOTP;
--- Frontend/src/pages/OTP/VerifyOTP.jsx
+++ Frontend/src/pages/OTP/VerifyOTP.jsx
@@ -1,28 +1,55 @@
 import { useNavigate } from "react-router";
 import { useState } from "react";
+import { useAuth } from "../../context/AuthContext";
+import axios from "axios";
+import Swal from 'sweetalert2';
 
 const VerifyOTP = () => {
     const navigate = useNavigate();
+    const { currentUser } = useAuth(null);
+    const email = currentUser.providerData[0].email;
     const [otp, setOtp] = useState("");
 
-    const checkOTP = () => {
-        navigate("/");
-    }
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer);
+            toast.addEventListener('mouseleave', Swal.resumeTimer);
+        }
+    });
 
-    return <>
-        <h2>Let's verify your OTP</h2>
+    const checkOTP = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/otp/verify-otp", {
+                email,
+                code: otp,
+            });
+            console.log(response.data.message);
+            navigate("/");
+            Toast.fire({
+                icon: "success",
+                title: `${currentUser.providerData[0].email} Logged In Successfully`
+            });
+        } catch (err) {
+            Toast.fire({
+                icon: "error",
+                title: "Invalid or expired OTP"
+            });
+            console.error(err.response?.data || err.message);
+        }
+    };
 
-        <input
-            type="text"
-            placeholder="Enter OTP"
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-        />
+    return <div className="w-[50vw] h-[50vh] mx-auto mt-[25vh] flex flex-col justify-evenly items-center">
+        <h2 className="text-5xl font-semibold">Let's verify your OTP</h2>
 
-        <button onClick={checkOTP}>
-            Check OTP
-        </button>
-    </>
+        <input className="h-[6vh] pl-3 w-[20vw] border-2 rounded-2xl text-xl" type="text" placeholder="Enter OTP" value={otp} onChange={(e) => setOtp(e.target.value)} />
+
+        <button onClick={checkOTP} className="w-[8vw] h-[6vh] bg-yellow-400 text-xl font-semibold flex justify-center items-center rounded-md py-1 hover:scale-115 cursor-pointer"> Check OTP </button>
+    </div>
 };
 
 export default VerifyOTP;
